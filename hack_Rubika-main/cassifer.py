e8e73f7f7bb8836cb149b085ee62f4bee556d4e0
amirbendaz0711 <94342413+amirbendaz0711@users.noreply.github.com>
2022-04-16 18:22:59 +0430

    Add files via upload

---
+++ hack_Rubika-main/cassifer.py
@@ -0,0 +1,191 @@
+# MR M.T.H ☘️ 
+#this is bug !                                            # slm                                                           #import library's
+import os
+# auto install libs
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing libs
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# code colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+#$CRIPT
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+
+#date()
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{blue} ")
+print ("         Script By")
+print (Fore.YELLOW + "")
+x = """ MR M.T.H  ....
+
+.                                                      .
+      .n                     .             .                n.
+  . .dP                   dP               9b               9b   .
+ 4  qXb         .        dX                 Xb       .      dXp   t
+dX.  9Xb      .dXb     __                     __    dXb.   dXP   .Xb
+9XXb._     _.dXXXXb dXXXXbo.               dXXXXb dXXXXb._   _.dXXP
+ 9XXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXP
+   9XXXXXXXXXXXXXXX ~   ~ OOO8b   d8OOO ~   ~ XXXXXXXXXXXXXXXXXP
+     9XXXXXP   9XX      *     98v8P      *     XXP   9XXXXXXXP
+      ~~~       9X.          .db|db.          .XP       ~~~
+                  )b.  .dbo.dP' v  9b.odb.  .dX(
+                ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+               dXXXXXXXXXXXP'   .    9XXXXXXXXXXXb
+              dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+              9XXb'    XXXXXb.dX|Xb.dXXXXX'    dXXP
+                       9XXXXXX(   )XXXXXXP
+                       XXXX X. v .X XXXX
+                        XP^X' b   d' X^XX
+                        X. 9         P )X
+                         b          '  d'
+
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+#⁶6⁶
+print(f"{red} ")
+d = """
+
+
+                            ⁶6⁶
+          ▄▀▄     ▄▀▄
+         ▄█░░▀▀▀▀▀░░█▄
+     ▄▄  █░░░░░░░░░░░█  ▄▄
+    █▄▄█ █░░█░░┬░░█░░█ █▄▄█
+
+                     !!!! Script is Mr M.T.H
+
+
+Filtering RUBIKA running //////////////////////////////////////////////////////////////////////////.......
+"""
+for d in d:
+        sys.stdout.write(d)
+        sys.stdout.flush()
+        time.sleep(0.03)
+# M.T.H *_*
+#my cat
+#          ▄▀▄     ▄▀▄
+#         ▄█░░▀▀▀▀▀░░█▄
+#     ▄▄  █░░░░░░░░░░░█  ▄▄
+#    █▄▄█ █░░█░░┬░░█░░█ █▄▄█
+
+time.sleep(4.3)
+print(f"{green} ")
+print ("Hello My frends M.T.H  . 💙 . ")
+print (Fore.YELLOW + "")
+time.sleep(1.6)
+print ("                         ID telegram @HACK_AMNIAT_TO")
+print(f"{blue} ")
+time.sleep(1.0)
+print ("__________")
+time.sleep(0.7)
+print ("#_________")
+time.sleep(0.6)
+print ("##________")
+time.sleep(0.5)
+print ("###_______")
+time.sleep(0.4)
+print ("####______")
+time.sleep(0.3)
+print ("#####_____")
+time.sleep(0.2)
+print ("######____")
+time.sleep(0.1)
+print ("#######___")
+time.sleep(0.5)
+print ("########__")
+time.sleep(0.1)
+print ("#########_")
+time.sleep(0.5)
+print ("##########")
+print ("")
+# vared kardane addad baraye gereftan kod
+up = input(" 2 Code channel & 3 for group & 4 for ac > > > ")
+print(f"{pink} ")
+time.sleep(3.0)
+print("""
+  update =
+
+$       cd $HOME
+
+$       rm -rf Filtering-Rubika
+
+$       git clone https://gitHub.com/C4ssif3r/Filtering-Rubika
+
+""")
+
+print(f"{red} ")
+
+
+
+if up == "2":
+
+        print ('code for Channel ≠>' + '%s%s %78ilter.%73 %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+if up != "2":
+        print("error - lotfan dorost vared konid")
+
+
+if up == "3":
+        print ('code group ≠>' + '%s%s (/*!50000(4.2.5.7/f////g.h/4.3.4.6.7.9.8.5.2.5.7.9.5.2.3.6.8.0.0.7.3.1/)) %s' % (fg('white'), bg('black'), attr('reset')))
+
+
+if up != "3":
+
+        print("error - lotfan dorost vared konid")
+
+
+if up == "4":
+
+        print ('code for account ≠>' + '%s%s http://ufhttk_26357 %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+
+if up != "4":
+        print("error - lotfan dorost vared konid")
+
+time.sleep(0.5)
+print(f"{green}" )
+print (Fore.WHITE + " ")
+time.sleep(3.0)
+print (" Bye Bye My Friend :)💔")
+time.sleep(3.0)
+print (f"{red} ")
+time.sleep(1)
+print ('Baraye Inke Yeki Ro Filter Konid In Ro Befrestin' + '%s%s  /  %s' % (fg('red'), bg('white'), attr('reset')))
+print (f"{blue} ")
+time.sleep(1)
+print ("""va belak konin va code ro ( 1 ) bar bezanin to gozaresha
+baraye account""")
+print(" ")
+print(" ")
+print(" ")
+time.sleep(2.0)
+os.system("cd $HOME")
+os.system("rm -rf /data/data/com.termux/files/home/Filtering-Rubika && git clone https://github.com/C4ssif3r/Filtering-Rubika")
+print ("time end :")
+print (Fore.WHITE + "")
+print ('\n\r HACK M.T.H')
+date()
